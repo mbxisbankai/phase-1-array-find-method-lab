e23562efd247c979cefdc12f7f6634d681103a3e
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-03-10 18:08:39 +0300

    Array find method Lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+const record = [
+    { 
+        year: "2015", 
+        result: "W",
+    },
+    { 
+        year: "2014", 
+        result: "N/A",
+    },
+    { 
+        year: "2013", 
+        result: "L",
+    },
+];
+
+function superbowlWin(array){
+    for(const obj of array){
+        if(obj.result === 'W'){
+            return obj.year;
+            break;
+        }
+    }
+}
+superbowlWin(record);
